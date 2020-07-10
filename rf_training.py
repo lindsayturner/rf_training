73eb76eee1bdbcf070bbafb4cad07a281af56a2f
lindsayturner <lindsay.c.turner1@gmail.com>
2020-07-10 12:06:37 -0700

    Update rf headers

--- rf_training.py
+++ rf_training.py
@@ -2,23 +2,25 @@
 """
 Created on Thu Jul  2 19:53:05 2020
 
-@author: linds
-Code 
+@author: Lindsay Turner
+
+Random Forest Model 
 """
 
 ##############################################################################
-# IMPORT
+# IMPORT PACKAGES
 ##############################################################################
 
 import pandas as pd
 import numpy as np
-#import shutil
 import random
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.inspection import permutation_importance
 
 ##############################################################################
 # FUNCTIONS
@@ -36,10 +38,22 @@ def undersample_ds(x, classCol, nsamples_class):
             xMatch = x[(x[classCol]).str.match(i)]
             x = x.drop(xMatch.sample(n = len(xMatch) - nsamples_class).index)
     return x
-        
 
+
+def string_to_int(y):
+    unique_y = np.unique(y)
+    y = y.to_numpy()
+    for i in range(len(y)):
+        for j in range(len(unique_y)):
+            if(y[i] == unique_y[j]):
+                y[i] == j
+    y = y.astype('int')
+    unique_y = unique_y.astype('int')
+    return y, unique_y
+   
+     
 ##############################################################################
-# IMPORT DATA & CREATE INDICES
+# IMPORT DATA & CREATE DATAFRAME
 ##############################################################################
     
 dfAll = pd.read_csv(r'C:/Users/linds/NOAA/rf_training/data_raw/training_data_1M_sub.csv')
@@ -81,26 +95,34 @@ indices_df = indices_df * 10000
 indices_df['Classname'] = pd.Series(training_bc['Classname'],
                                     index = indices_df.index)
 
-#features = indices_df.iloc[:, [i for i in range(indices_df.shape[1]) if i != 15]]
+##############################################################################
+# RANDOM FOREST MODEL
+##############################################################################
+
+# X data for rf
 features = indices_df
-labels = (indices_df['Classname']).to_numpy()
-unique_labels = np.unique(indices_df['Classname'])
-for i in range(len(labels)):
-    for j in range(len(unique_labels)):
-        if(labels[i] == unique_labels[j]):
-            labels[i] = j
+
+# y data for rf
+labels, labels_name = string_to_int(indices_df['Classname'])
 
 #need an array of integers representing classnames to run
 
-X_train, X_test, y_train, y_test = train_test_split(features[feature_names], labels, train_size = 0.9, random_state = 42, stratify = labels)
+X_train, X_test, y_train, y_test = train_test_split(features[feature_names],
+                                                    labels,
+                                                    train_size = 0.9,
+                                                    random_state = 42,
+                                                    stratify = labels)
 
-rf = RandomForestClassifier(n_estimators = 200, random_state = 8)
+rf = RandomForestClassifier(n_estimators = 200,
+                            max_features = 5,
+                            random_state = 8)
 
 rf.fit(X_train, y_train)
 
 predictions = rf.predict(X_test)
 errors = abs(predictions - test_labels)
 accuracy = accuracy_score(y_test, predictions)
+confusionmatrix = pd.DataFrame(confusion_matrix(y_test, predicted),
+                               columns=labels_name,
+                               index=labels_name)
 
-predicted = rf.predict(X_test)
-accuracy = accuracy_score(y_test, predicted)
